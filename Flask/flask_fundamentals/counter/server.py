9e32ef4071ab3b01b98876b847e1cb19a3fb87d4
Nasser Sayeh <nasser.sayeh89@gmail.com>
2021-05-16 17:19:11 +0300

    counter

---
+++ Flask/flask_fundamentals/counter/server.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template,request,session,redirect
+
+app = Flask(__name__)    
+app.secret_key = 'nasser sayeh' 
+@app.route('/')          
+def one():
+    if 'count' not in session:
+        session['count']=0
+    session['count']+=1
+    return render_template("index.html",count=session['count'])  
+@app.route('/addtwo')          
+def three():
+    if 'count' not in session:
+        session['count']=0
+    session['count']+=2
+    if 'sik' in session:
+        session.pop('sik')
+    return render_template("index.html",count=session['count'])  
+@app.route('/destroy_session')          
+def two():
+    session.clear()
+    return redirect('/')
+
+@app.route('/addx',methods=["POST"])          
+def four():
+    print(request.form['num'])
+    if 'count' not in session:
+        session['count']=0
+    session['count']+=int(request.form['num'])
+    session['sik']=request.form['num']
+    return redirect('/')
+if __name__=="__main__":
+    app.run(debug=True)
