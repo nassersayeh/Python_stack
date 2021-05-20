da79de74a18d438b6db31537667aeb901240e520
Nasser Sayeh <nasser.sayeh89@gmail.com>
2021-05-20 14:49:32 +0300

    user with templates

--- django/django_into/user/my_app/views.py
+++ django/django_into/user/my_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+# other imports
+from .models import user
+# show all of the data from a tablecopy
+def index(request):
+    context = {
+        "all_the_user": user.objects.all()
+    }
+    return render(request, "index.html", context)
 
-# Create your views here.
+def adduser(request):
+    user.objects.create(firstname = request.POST['firstname'] , lastname = request.POST['lastname'] , email = request.POST['email'], age = request.POST['age'])
+    return redirect('/')
