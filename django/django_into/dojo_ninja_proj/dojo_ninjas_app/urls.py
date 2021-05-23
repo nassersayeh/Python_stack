96e29f63ed17c5ead273ceb4b5a2f796febfe4bf
Nasser Sayeh <nasser.sayeh89@gmail.com>
2021-05-23 15:36:32 +0300

    dojos and ninjas form

--- django/django_into/dojo_ninja_proj/dojo_ninjas_app/urls.py
+++ django/django_into/dojo_ninja_proj/dojo_ninjas_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('adddojo',views.AddDojo),
+    path('addninja', views.AddNinja),
+    
     
 ]
--- django/django_into/dojo_ninja_proj/dojo_ninjas_app/views.py
+++ django/django_into/dojo_ninja_proj/dojo_ninjas_app/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
 
 # Create your views here.
 def index(req):
-    return render(req,"index.html")
+    context = {
+        "dojonames":Dojo.objects.all(),
+        "dojos" : Dojo.objects.all(),
+        "ninjas":Ninjas.objects.all()
+    }
+    return render(req,"index.html",context)
+def AddDojo(req):
+    Dojo.objects.create(name = req.POST['name'], city = req.POST['city'],state = req.POST['state'])
+    return redirect('/')
+def AddNinja(req):
+    Ninjas.objects.create(first_name= req.POST['firstname'],last_name = req.POST['lastname'], dojo = Dojo.objects.get(name=req.POST['dojo']))
+    return redirect('/')
