0f96875c39b5c750b2134f2ff2c38cc7ba43af23
Nasser Sayeh <nasser.sayeh89@gmail.com>
2021-05-26 00:29:31 +0300

    update

--- django/django_into/semi_restfulltv_show/my_app/models.py
+++ django/django_into/semi_restfulltv_show/my_app/models.py
@@ -1,11 +1,26 @@
-from django.db import models 
+from django.db import models
+import datetime
+
+class BlogManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['showtitle']) < 2:
+            errors["showtitle"] = "Show Title should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Show Network should be at least 3 characters"
+        if len(postData['desc']) < 10:
+            errors["desc"] = "Show Description should be at least 10 characters"
+        if postData['releasedate'] >= str(datetime.date.fromisocalendar):
+            errors["releasedate"] = "Show Release Date should be at least in the past"
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=55)
     releasedate=models.DateField()
     desc = models.TextField()
-    
+    objects = BlogManager()
 
 
 
--- django/django_into/semi_restfulltv_show/my_app/views.py
+++ django/django_into/semi_restfulltv_show/my_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django.contrib import messages
 from .models import*
 
 def index(req):
@@ -9,14 +10,25 @@ def index(req):
 def show(req):
     return render(req,"addshow.html")
 def addshow(req):
-    title = req.POST['showtitle']
-    network = req.POST['network']
-    releasedate = req.POST['releasedate']
-    desc = req.POST['desc']
-    Show.objects.create(title=title,network=network,releasedate=releasedate,desc=desc)
-    last = Show.objects.last()
-    id = last.id
-    return redirect('showtv/'+str(id))
+    # pass the post data to the method we wrote and save the response in a variable called errors
+    errors = Show.objects.basic_validator(req.POST)
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(req, value)
+        # redirect the user back to the form to fix the errors
+        return redirect('/show')
+    else:
+        title = req.POST['showtitle']
+        network = req.POST['network']
+        releasedate = req.POST['releasedate']
+        desc = req.POST['desc']
+        Show.objects.create(title=title,network=network,releasedate=releasedate,desc=desc)
+        last = Show.objects.last()
+        id = last.id
+        messages.success(req, "Show successfully updated")
+        return redirect('showtv/'+str(id))
 def showtv(req,id):
     showid = Show.objects.get(id=id)
     
@@ -37,11 +49,21 @@ def showedit(req,id):
     } 
     return render(req,"editshow.html",context)
 def edit(req,id):
+    # pass the post data to the method we wrote and save the response in a variable called errors
+    errors = Show.objects.basic_validator(req.POST)
+    last = Show.objects.get(id=id)
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(req, value)
+        # redirect the user back to the form to fix the errors
+        return redirect('/showtv/'+str(last.id)+'/edit')
     title = req.POST['showtitle']
     network = req.POST['network']
     releasedate = req.POST['releasedate']
     desc = req.POST['desc']
-    last = Show.objects.get(id=id)
+    
     Show.objects.filter(id=last.id).update(title=title,network=network,releasedate=releasedate,desc=desc)
     last2 = Show.objects.get(id=id)
     context ={
@@ -50,10 +72,31 @@ def edit(req,id):
         'showdesc':last2.desc,
         'showreles':last2.releasedate,
         
-    } 
+    }
+    messages.success(req, "Show successfully updated")
     return render(req,"showtv.html",context)
 def destroy(req,id):
     Show.objects.filter(id=id).delete()
     return redirect('/shows')
 
-
+def update(request, id):
+    # pass the post data to the method we wrote and save the response in a variable called errors
+    errors = Show.objects.basic_validator(request.POST)
+        # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect('/shows')
+    else:
+        # if the errors object is empty, that means there were no errors!
+        # retrieve the blog to be updated, make the changes, and save
+        show = Show.objects.get(id = id)
+        show.title = request.POST['showtitle']
+        show.network = request.POST['network']
+        show.desc = request.POST['desc']
+        show.save()
+        messages.success(request, "Show successfully updated")
+        # redirect to a success route
+        return redirect('/showtv/'+str(show.id))
